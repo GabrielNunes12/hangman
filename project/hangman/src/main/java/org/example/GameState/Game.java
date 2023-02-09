b622366dcd975122e2633b20498da1d3b54d66e5
Gabriel <prismaticnetwork12@gmail.com>
2023-02-09 19:51:57 -0300

    fix: adjusting classes to work properly

--- project/hangman/src/main/java/org/example/GameState/Game.java
+++ project/hangman/src/main/java/org/example/GameState/Game.java
@@ -1,7 +1,9 @@
 package org.example.GameState;
 
 import org.example.Entities.Tasks;
+import org.example.Entities.Words;
 
+import java.util.Scanner;
 import java.util.Timer;
 
 public class Game {
@@ -10,13 +12,26 @@ public class Game {
   static Tasks task = new Tasks(tryOut, timer);
   static Timer timerCounter = new Timer();
   private static boolean hasStarted;
-
-  public void main(String[] args) {
+  private Words words = new Words();
+  private String secretWord = "_".repeat(words.getWord().length());
+  public void main() {
+    Scanner scanner = new Scanner(System.in);
     if(hasStarted) {
-      try {
-        timerCounter.scheduleAtFixedRate(task, 0, 60000);
-      } catch (Exception exception) {
-        System.out.println(exception.getMessage());
+      while(!words.getWord().equals(secretWord)) {
+        System.out.println("Guess a word: ");
+        String letter = scanner.nextLine();
+        //looping for every secret words
+        for(int i = 0; i < secretWord.length(); i++) {
+          if(words.getWord().charAt(i) == letter.charAt(0)) {
+            secretWord = secretWord.substring(0, i) + letter + secretWord.substring(i, 1);
+          }
+        }
+        System.out.println(secretWord);
+        if(secretWord.contains(letter)) {
+          System.out.println("You guessed it right!");
+        } else {
+          System.out.println("You guessed it wrong!");
+        }
       }
     } else {
       System.out.println("Game has not started yet!");
@@ -37,6 +52,6 @@ public class Game {
     tryOut = 1;
   }
   public void setHasStarted(boolean hasStarted) {
-    hasStarted = hasStarted;
+    this.hasStarted = hasStarted;
   }
 }
